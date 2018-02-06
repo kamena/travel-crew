b912e0906fed6a1bb6681a3bcc2bfa3adc6ae966
kamena <kamena.dacheva@gmail.com>
2018-02-06 13:09:58 +0200

    Small updates

--- js/script.js
+++ js/script.js
@@ -9,28 +9,21 @@ jQuery(document).ready(function () {
 	    }
 	});
 
-	
-
-	if(jQuery('body').is('.home')) {
+	if(jQuery("body").is(".home")) {
 		var slideIndex = 0;
 		carousel();
 	}
 
-	function carousel(className) {
+	function carousel() {
 	    var i;
 	    var x = document.getElementsByClassName("slider");
 	    for (i = 0; i < x.length; i++) {
 	      x[i].classList.add("hidden"); 
 	    }
 	    slideIndex++;
-	    if (slideIndex > x.length) {slideIndex = 1} 
+	    if (slideIndex > x.length) { slideIndex = 1; } 
 	    x[slideIndex-1].classList.remove("hidden"); 
 	    setTimeout(carousel, 5000); 
-	}
-
-	function openMenu() {
-	   var element = document.getElementsByClassName("nav-right");
-	   element.classList.add("hidden");
 	}
 
 	document.getElementById("menu-toggle").onclick = function() {
@@ -42,13 +35,13 @@ jQuery(document).ready(function () {
 	    }
 	};
 
-    jQuery('#login-form-link').click(function(e) {
+    jQuery("#login-form-link").click(function(e) {
 		jQuery("#login-form").delay(100).fadeIn(100);
  		jQuery("#register-form").fadeOut(100);
 		e.preventDefault();
 	});
 
-	jQuery('#register-form-link').click(function(e) {
+	jQuery("#register-form-link").click(function(e) {
 		jQuery("#register-form").delay(100).fadeIn(100);
  		jQuery("#login-form").fadeOut(100);
 		e.preventDefault();
